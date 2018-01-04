64f59d95c9ae56555f509c71212c8cc73a90094f
Linus Groh <mail@linusgroh.de>
2018-01-04 19:07:33 +0100

    Prepare for initial release

--- CHANGES.md
+++ CHANGES.md
@@ -1,1 +1,9 @@
-_Nothing released yet._
+# Release history
+
+## `1.0.0` (04.01.2017)
+
+- Initial release
+- Added working code for all buttons, axes and hat
+- Added rumbling and led support
+- Added some documentation
+- Released on GitHub and PyPI
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from xbox360controller import __version__
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='xbox360controller',
+    packages=['xbox360controller', 'xbox360controller.linux'],
+    version=__version__,
+    description='A pythonic Xbox360 controller API built on top of the xpad '
+                'Linux kernel driver.',
+    long_description=long_description,
+    author='Linus Groh',
+    author_email='mail@linusgroh.de',
+    license='MIT',
+    url='https://github.com/linusg/xbox360controller',
+    download_url='https://pypi.python.org/pypi/xbox360controller/',
+    keywords=['xbox', 'xbox360', 'controller', 'gamepad', 'game',
+              'raspberry pi', 'event', 'led', 'rumble'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System',
+        'Topic :: System :: Hardware',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+        'Topic :: Utilities'
+    ],
+)
