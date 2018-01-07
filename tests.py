78bfd87eaf643f53197a7338ba9215dcec9a84db
Linus Groh <mail@linusgroh.de>
2018-01-07 14:52:37 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,12 @@
+import unittest
 
+import xbox360controller
+
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(xbox360controller.Xbox360Controller.LED_OFF, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
